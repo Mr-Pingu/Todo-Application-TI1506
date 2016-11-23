8b5767a209186b9c6b82ca370f80a99d531ad700
JorisJonk <joris33@gmail.com>
2016-11-23 17:29:19 +0100

    script added

---
+++ js/todoscript.js
@@ -0,0 +1,44 @@
+var array = array();
+
+
+function loginuser(form){
+  var un = document.logininfo.usr.value;
+  var pw = document.logininfo.pword.value;
+
+  console.log(un);
+  console.log(pw);
+  window.location="todolist.html";
+
+
+}
+
+function addtodo(naam, prio, datum, af){
+  var task = {name: naam, priority: prio, date: datum, done:af}
+  array.push(task);
+}
+
+function editprio(index){
+  if (array[index] == 0){
+  array[index].priority = 1;}
+  else {array[index].priority = 0;}
+}
+
+function editname(newnaam){
+  if (naam !== null){
+  array[index].name = naam;}
+  else {return false;}
+
+}
+
+function editdate(newdate){
+  if (newdate !== 0) {
+    array[index].date = newdate;}
+    else { return false;}
+  }
+
+
+function editdone(newdone){
+  if (array[index] == 0){
+  array[index].priority = 1;}
+  else {array[index].priority = 0;}
+}
