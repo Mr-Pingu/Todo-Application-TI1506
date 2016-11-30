5be3251f3a702657232fd1ec8b8c8dfb9e6a3df8
JorisJonk <joris33@gmail.com>
2016-11-30 15:55:56 +0100

    check

--- js/todoscript.js
+++ js/todoscript.js
@@ -1,5 +1,5 @@
 var todolist = [];
-
+var index = "";
 
 
 
@@ -17,12 +17,12 @@ function loginuser(form){
 
 function addtodo(form){
   var name = form.taskname.value;
-  var prio = form.priority.value;
+  var prio = form.priority.checked;
   var date = form.date.value;
-  var due = form.done.value;
+  var due = form.done.checked;
   var task = [name,prio,date,due];
   todolist.push(task);
-  console.log(todolist[0]);
+  console.log(todolist[todolist.length-1]);
 }
 
 
@@ -77,29 +77,76 @@ function editdone(newdone){
 }
 
 function showtodofromarray(array) {
-  for(i = 0;i < todolist.length; i==)
+  for(i = 0;i < todolist.length; i++){
   var tablerow = document.createElement("tr");
   var tabledata1 = document.createElement("td");
-  var node = document.createTextNode(array[0][0]);
+  var node = document.createTextNode(array[i][0]);
   tabledata1.appendChild(node);
   var tabledata2 = document.createElement("td");
-  var node2 = document.createTextNode(array[0][1]);
+  var node2 = document.createTextNode(array[i][1]);
     tabledata2.appendChild(node2);
   var tabledata3 = document.createElement("td");
-  var node3 = document.createTextNode(array[0][2]);
+  var node3 = document.createTextNode(array[i][2]);
     tabledata3.appendChild(node3);
   var tabledata4 = document.createElement("td");
-  var node4 = document.createTextNode(array[0][3]);
+  var node4 = document.createTextNode(array[i][3]);
     tabledata4.appendChild(node4);
+  //  var tabledata5 = document.createElement("td");
+  //  var node5 = document.createElement("button");
+//tabledata5.appendChild(node5);
 
   tablerow.appendChild(tabledata1);
   tablerow.appendChild(tabledata2);
 tablerow.appendChild(tabledata3);
 tablerow.appendChild(tabledata4);
+//tablerow.appendChild(tabledata5);
 
 var element = document.getElementById("t_newadd");
 element.appendChild(tablerow);
+}
 
 
     //chatlog.scrollTop = chatlog.scrollHeight;
+}
+
+function showeditbutton(int){
+ document.getElementById('editshow').style.display = "block";
+ index = int-1;
+  console.log(index);
+}
+
+function hideeditbutton(){
+   document.getElementById('editshow').style.display = "none";
+}
+
+function changetodo(form){
+  console.log("pre")
+  console.log(todolist)
+var name = form.taskname.value;
+var prio = form.priority.value;
+var date = form.date.value;
+var due = form.done.value;
+var task = [name,prio,date,due];
+console.log('task')
+console.log(task);
+console.log(index);
+todolist[index] = task;
+console.log(todolist[index]);
+hideeditbutton();
+console.log("post")
+console.log(todolist)
+}
+
+function removeoldtodos() {
+    var checkp = document.getElementById("chatlog");
+    var checkc = document.getElementById("n1");
+
+
+    while (checkc !== null) {
+        var parent = document.getElementById("chatlog");
+        var child = document.getElementById("n1");
+
+        parent.removeChild(child);
+        checkc = document.getElementById("n1");
+    }
 }
