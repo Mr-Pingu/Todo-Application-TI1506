55dd88d321a3a98bd0e623e116c650742f1e92dd
Mr-Pingu <karstenvanherwaarden@gmail.com>
2016-12-14 16:01:55 +0100

    server update

--- todo-server.js
+++ todo-server.js
@@ -1,3 +1,5 @@
+/* global Buffer */
+/* global __dirname */
 var express = require("express");
 var url = require("url");
 var http = require("http");
@@ -6,42 +8,68 @@ var port = 3000;
 var app = express();
 app.use(express.static(__dirname));
 http.createServer(app).listen(port);
-console.log("server listening");
 
 var todos = [];
-var t1 = { task : "Maths homework due", type  : 1, deadline : "12/12/2015"};
-var t2 = { task : "English homework due", type : 3, deadline : "20/12/2015"};
+var t1 = { task : "Maths homework due", prio  : 1, date : "12/12/2015", done : "false" };
+var t2 = { task : "English homework due", prio : 3, date : "20/12/2015", done : "false"};
 todos.push(t1);
 todos.push(t2);
 
-// list todos array
+//clients requests todos
 app.get("/todos", function (req, res) {
 	console.log("todos requested!");
 	res.json(todos);
 });
 
+//add todo to the server
 app.get("/addtodo", function (req, res) {
 	var url_parts = url.parse(req.url, true);
 	var query = url_parts.query;
-    console.log(test);
-	
+
 	if(query["task"]!==undefined) {
-		var tx = { 
-			task : query["task"], 
-			prio: query["type"],
-			date: query["deadline"],
-            done: false
+		var tx = { task : query["task"],
+			prio: query["prio"],
+			date: query["date"],
+			done: query["done"]
 		};
 		todos.push(tx);
 		console.log("Added " + tx.task);
-		res.end("Todo added successfully");
+		res.end("Todo addeds successfully");
 	}
 	else {
-		res.end("Error: missing message parameter");
+		res.end("Error: missing task parameter");
 	}
 });
 
-app.get("/remove", function (reg, res) {
+app.get("/edittodo", function (req, res) {
+	var url_parts = url.parse(req.url, true);
+	var query = url_parts.query;
+  var index =  query["index"];
+	console.log(index);
+	if(query["task"]!==undefined) {
+		var tx = { task : query["task"],
+			prio: query["prio"],
+			date: query["date"],
+			done: query["done"]
 
-})
+		};
+		todos[index] = tx;
+		console.log("edited " + tx.task + index);
+		res.end("Todo edited successfully");
+	}
+	else {
+		res.end("Error: missing task parameter");
+	}
+});
 
+app.get("/removetodo", function (req, res) {
+	var url_parts = url.parse(req.url, true);
+	var query = url_parts.query;
+  var index = query["index"];
+	console.log(index);
+
+		todos.splice(index,1);
+		console.log("removed + index");
+		res.end("Todo removed successfully");
+
+});
